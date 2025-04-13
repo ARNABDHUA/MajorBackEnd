acc881a5914d0f10d2768a4fab8cd858f5e723bf
ARNABDHUA <arnabdhua74@gmail.com>
2025-04-13 23:26:15 +0530

    port

--- index.js
+++ index.js
@@ -144,9 +144,17 @@ app.post("/verify", async (req, res) => {
   }
 });
 
-const PORT = 3000;
-const server=app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// const PORT = 3000;
+// const server=app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+
+const PORT = process.env.PORT || 10000;
+
+// MUST bind to 0.0.0.0
+const server=app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 
